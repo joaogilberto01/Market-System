b0766bdd23cc657c1c6e2906dedb5f727c7b21c2
joaogilberto01 <joaogilbertomelo06@gmail.com>
2025-09-11 21:54:20 -0300

    mudança na interface dos menus

--- metodos/menu.py
+++ metodos/menu.py
@@ -1,6 +1,10 @@
+def cabecalho():
+    print("="*20,"| Mercado Big3 |","="*20)
+    
 #função que registra o usuario com nome e senha em um arquivo .txt
 def registros():
-    print("="*20,"| Cadastramento => Registro |","="*20)
+    cabecalho()
+    print("|Cadastramento => Registro")
     cadastro = open("Arquivos/Usuarios.txt", "a")
     nome = input("Digite seu nome: ").strip()
     senha = input("Digite sua senha: ").strip()
@@ -11,7 +15,8 @@ def registros():
 
 # função que chama o aquivo usuario é le se ele existe!
 def login():
-    print("="*20,"| Cadastramento => Login |","="*20)
+    cabecalho()
+    print("| Cadastramento => Login")
     cadastro = open("Arquivos/Usuarios.txt", "r")
     nome = input("digite o nome: ").strip()
     senha = input("digite a senha: ").strip()
@@ -19,19 +24,20 @@ def login():
         linha_lida = linha.strip().split("#")
         if linha_lida[0] == nome and linha_lida[1] == senha:
             cadastro.close()
-            print(f"Seja Bem Vindo {linha_lida[0]}")
+            print(f"Seja Bem Vindo {linha_lida[0].capitalize()} !")
             return True
 
 def menu_principal():
     while True:
-        print("="*20,"| Bem Vindo ao Mercado Big3 |","="*20)
-        numero = int(input("1-Compra\n2-Historico\n3-Sair\n "))
+        cabecalho()
+        numero = int(input("1-Compra\n2-Historico\n3-Sair\n"))
         if numero > 0:
             break
 
 def menu_cadastrar():
     while True:
-        print("="*20,"| Cadastramento |","="*20)
+        cabecalho()
+        print("| Cadastramento")
         numero = int(input("1-Registrar\n2-Login\n3-Anônimo\n4-Sair\n"))
         if numero == 1:
             registros()
